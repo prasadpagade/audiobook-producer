735a2d085d542cd36fc30829eb0bcb44b10bdda4
Prasad Pagade <prasad.pagade@gmail.com>
2025-11-11 16:39:00 -0700

    Adjust CORS allowlist

--- backend/server.py
+++ backend/server.py
@@ -33,12 +33,13 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "https://audiobook-producer.vercel.app",
         "https://audiobook-producer-git-main-prasads-projects-9efa731b.vercel.app",
-        "https://audiobook-producer-19wdv8nwg-prasads-projects-9efa731b.vercel.app"
+        "https://audiobook-producer-19wdv8nwg-prasads-projects-9efa731b.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # Directory for generated audio
